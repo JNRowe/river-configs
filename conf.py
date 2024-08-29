d2e654ccf74b4443564e8389b3d127992f421012
James Rowe <jnrowe@gmail.com>
2024-08-29 06:24:07 +0100

    feat: Convert build to use Sphinx
    I’m pretty happy with river at this point, so it looks like it will be sticking
    around. The biggest change that means is that I would like to start referencing
    the river configuration from within my other documentation sets, and switching
    to Sphinx makes that easy.
    
    Thanks Sphinx people!

---
+++ conf.py
@@ -0,0 +1,57 @@
+#
+"""conf - Sphinx configuration information."""
+
+import os
+from contextlib import suppress
+from subprocess import CalledProcessError, PIPE, run
+
+import sphinx_rtd_theme
+
+on_github = "GITHUB_ACTIONS" in os.environ
+
+# General configuration {{{
+needs_sphinx = "4.3"
+
+exclude_patterns: [str] = [
+    ".build",
+    ".github",
+    "maybe",
+    "README.rst",
+]
+# }}}
+
+# Project information {{{
+project = "river-configs"
+copyright = "2024  James Rowe"  # NOQA: A001
+
+if on_github:
+    with suppress(CalledProcessError):
+        proc = run(
+            [
+                "git",
+                "-C",
+                os.path.dirname(__file__),
+                "log",
+                "--pretty=format:%ad [%h]",
+                "--date=short",
+                "-n1",
+            ],
+            stdout=PIPE,
+        )
+        html_last_updated_fmt = proc.stdout.decode()
+else:
+    # Use a static updated time to limit rebuilds for faster commit hooks
+    html_last_updated_fmt = "[local build]"
+
+release = html_last_updated_fmt
+version = ""
+# }}}
+
+# Options for HTML output {{{
+html_theme = "sphinx_rtd_theme"
+html_theme_path: [str] = [
+    sphinx_rtd_theme.get_html_theme_path(),
+]
+
+highlight_language = "zsh"
+# }}}
